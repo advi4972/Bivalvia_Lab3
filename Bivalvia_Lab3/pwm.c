58a115df0f858cc91ad04c4e4735ede6398f8898
Adam Vitti <advi4972@colorado.edu>
2021-02-12 15:38:01 -0700

    Added interrupt stuff

--- Bivalvia_Lab3/pwm.c
+++ Bivalvia_Lab3/pwm.c
@@ -3,6 +3,7 @@
 
 
 void config_pwm_timer(void){
+    TIMER_A0->CTL |= TIMER_A_CTL_IE; //Turns on interrupts for clock
     TIMER_A0->CTL |= TIMER_A_CTL_TASSEL_2; //Chooses SMCLK as input clk
 
     //SET DIVIDER VALUE
@@ -25,10 +26,20 @@ void stop_pwm(void){
 }
 
 void config_pwm_gpio(void){
-    P2->DIR |= BIT4; //sets p2.4 as output
+    P2->DIR |= BIT4; //sets p2.4 as output 000010000
     P2->OUT &= ~BIT4; // sets bit4 to 0
 
-    P2->SEL0 |= BIT4;
-    P2->SEL1 &= ~BIT4;
+    P2->SEL0 |= BIT4; //0000100000
+    P2->SEL1 &= ~BIT4;// 111101111
+
+}
+
+void config_switch_interrupt(void){
+    P1->DIR &= ~BIT1; //sets p1.1 (push button) as input
+    P1->REN |= BIT1; //sets up pullup resistor config
+    P1->OUT |= BIT1; //Initially sets to 1 since it will be active low
+    P1->IE |= BIT1; //Enables interrupt functionality for P1.1
+
+    __NVIC_EnableIRQ(PORT1_IRQn);
 
 }
